a341a0eac223fef418a563d912884fe8f2c12507
vvv-e <vvedensky@subi.su>
2025-01-14 07:38:07 +0500

    Update module_01.py

--- module_01.py
+++ module_01.py
@@ -1,4 +1,4 @@
-# Домашнее задание по теме "Хендлеры обработки сообщений"
+# Домашнее задание по теме "Методы отправки сообщений"
 
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
@@ -11,12 +11,12 @@ dp = Dispatcher(bot, storage=MemoryStorage())
 
 @dp.message_handler(commands=['start'])
 async def all_massages(message):
-    print("Привет! Я бот помогающий твоему здоровью.")
+    await message.answer("Привет! Я бот помогающий твоему здоровью.")
 
 
 @dp.message_handler()
 async def all_massages(message):
-    print("Введите команду /start, чтобы начать общение.")
+    await message.answer("Введите команду /start, чтобы начать общение.")
 
 
 if __name__ == "__main__":
