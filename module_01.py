61605d3381e594b6064f01459891b07fdc2f229f
vvv-e <vvedensky@subi.su>
2025-01-14 07:08:05 +0500

    Update module_01.py

--- module_01.py
+++ module_01.py
@@ -2,10 +2,7 @@ from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 import asyncio
 
-api = ''.join(
-    ['7', '0', '8', '6', '3', '8', '0', '3', '3', '2', ':', 'A', 'A', 'H', '2', 'U', 'L', 'i', 'a', 'x', 'Q', 'S',
-     '6', 'S', 'R', 'c', 'G', '_', 'C', 's', 'c', '8', '2', 'K', 'I', 'M', 'D', 's', '1', 'T', '1', 'P', 'Z', 'Q',
-     'H', 'g'])
+api = 
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
