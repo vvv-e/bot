36d6ed2a2df69a0788941a129ae19493b64977f3
vvv-e <vvedensky@subi.su>
2025-01-14 07:42:51 +0500

    Update module_02.py

---
+++ module_02.py
@@ -0,0 +1,23 @@
+# Домашнее задание по теме "Методы отправки сообщений"
+
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+import asyncio
+
+api = "При отправке вашего кода на GitHub не забудьте убрать ключ для подключения к вашему боту!"
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+
+@dp.message_handler(commands=['start'])
+async def all_massages(message):
+    await message.answer("Привет! Я бот помогающий твоему здоровью.")
+
+
+@dp.message_handler()
+async def all_massages(message):
+    await message.answer("Введите команду /start, чтобы начать общение.")
+
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
