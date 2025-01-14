62b002042307a43816aebe4352c5f2d6f54708c0
vvv-e <vvedensky@subi.su>
2025-01-14 06:46:42 +0500

    Update module_01.py

--- module_01.py
+++ module_01.py
@@ -2,13 +2,18 @@ from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 import asyncio
 
-api = "7086380332"+":"+"AAH2ULiaxQS6SRcG_Csc82KIMDs1T1PZQHg"
+api = ''.join(
+    ['7', '0', '8', '6', '3', '8', '0', '3', '3', '2', ':', 'A', 'A', 'H', '2', 'U', 'L', 'i', 'a', 'x', 'Q', 'S',
+     '6', 'S', 'R', 'c', 'G', '_', 'C', 's', 'c', '8', '2', 'K', 'I', 'M', 'D', 's', '1', 'T', '1', 'P', 'Z', 'Q',
+     'H', 'g'])
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
+
 
 @dp.message_handler()
 async def all_message(message):
     print(f"Мы получили сообщение {message}")
+
 
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
