0c5fa7cae1eabfec82b57420ffa046335c48dbfe
vvv-e <vvedensky@subi.su>
2025-01-15 17:07:20 +0500

    Update module_03.py

---
+++ module_03.py
@@ -0,0 +1,62 @@
+# Домашнее задание по теме "Машина состояний"
+
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.dispatcher import FSMContext
+import asyncio
+
+api = ""  # "При отправке вашего кода на GitHub не забудьте убрать ключ для подключения к вашему боту!"
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+
+class UserState(StatesGroup):
+    age = State()  # возраст
+    growth = State()  # рост
+    weight = State()  # вес
+
+
+@dp.message_handler(commands=['start'])
+async def all_massages(message):
+    await message.answer("Привет! Я бот помогающий твоему здоровью.")
+
+
+@dp.message_handler(text="Calories")
+async def get_age(message):
+    await message.answer("Введите свой возраст:")
+    await UserState.age.set()
+
+
+@dp.message_handler(state=UserState.age)
+async def set_growth(message, state):
+    await state.update_data(age=message.text)
+    data = await state.get_data()
+    await message.answer(f"Ваш возраст {data['age']}")
+    await message.answer("Введите свой рост:")
+    await UserState.growth.set()
+
+
+@dp.message_handler(state=UserState.growth)
+async def set_growth(message, state):
+    await state.update_data(growth=message.text)
+    data = await state.get_data()
+    await message.answer(f"Ваш рост {data['growth']}")
+    await message.answer("Введите свой вес:")
+    await UserState.weight.set()
+
+
+@dp.message_handler(state=UserState.weight)
+async def set_growth(message, state):
+    await state.update_data(weight=message.text)
+    data = await state.get_data()
+    await message.answer(f"Ваша вес {data['weight']}")
+    await message.answer(
+        f"Ваша норма калорий:{10 * int(data['weight']) + 6.25 * int(data['growth']) - 5 * int(data['age']) + 5}")
+    await UserState.growth.set()
+
+
+# await state.finish()
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
