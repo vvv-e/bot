862c3a98a45ee4548e55900573211b3911dd312b
vvv-e <vvedensky@subi.su>
2025-01-16 00:29:36 +0500

    Update module_03.py

--- module_03.py
+++ module_03.py
@@ -6,7 +6,7 @@ from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.dispatcher import FSMContext
 import asyncio
 
-api = ""  # "При отправке вашего кода на GitHub не забудьте убрать ключ для подключения к вашему боту!"
+api = "При отправке вашего кода на GitHub не забудьте убрать ключ для подключения к вашему боту!"
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
@@ -31,8 +31,6 @@ async def get_age(message):
 @dp.message_handler(state=UserState.age)
 async def set_growth(message, state):
     await state.update_data(age=message.text)
-    data = await state.get_data()
-    await message.answer(f"Ваш возраст {data['age']}")
     await message.answer("Введите свой рост:")
     await UserState.growth.set()
 
@@ -40,8 +38,6 @@ async def set_growth(message, state):
 @dp.message_handler(state=UserState.growth)
 async def set_growth(message, state):
     await state.update_data(growth=message.text)
-    data = await state.get_data()
-    await message.answer(f"Ваш рост {data['growth']}")
     await message.answer("Введите свой вес:")
     await UserState.weight.set()
 
@@ -50,13 +46,9 @@ async def set_growth(message, state):
 async def set_growth(message, state):
     await state.update_data(weight=message.text)
     data = await state.get_data()
-    await message.answer(f"Ваша вес {data['weight']}")
     await message.answer(
         f"Ваша норма калорий:{10 * int(data['weight']) + 6.25 * int(data['growth']) - 5 * int(data['age']) + 5}")
-    await UserState.growth.set()
-
-
-# await state.finish()
+    await state.finish()
 
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
