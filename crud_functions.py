90a37060e9b4922944c105583252df78032d88ea
vvv-e <vvedensky@subi.su>
2025-01-27 06:00:46 +0500

    Update crud_functions.py

--- crud_functions.py
+++ crud_functions.py
@@ -1,51 +1,50 @@
 # Домашнее задание по теме "Выбор элементов и функции в SQL запросах"
 import sqlite3
 
-if __name__ == "__main__":
+
+def initiate_db():
     # Код из предыдущего задания
-    conn = sqlite3.connect("not_telegram.db")
+    conn = sqlite3.connect("bot.db")
     cursor = conn.cursor()
     #    создать таблицу
     cursor.execute('''
-    CREATE TABLE IF NOT EXISTS Users(
+    CREATE TABLE IF NOT EXISTS Products(
        id INTEGER PRIMARY KEY,
-       username TEXT NOT NULL,
-       email TEXT NOT NULL,
-       age INTEGER,
-       balance INTEGER NOT NULL
+       title TEXT NOT NULL,
+       description TEXT,
+       price INTEGER NOT NULL
        )
     ''')
+    conn.commit()
+    conn.close()
 
-    #    удалить записи, если есть
-    cursor.execute("DELETE FROM Users")
 
-    #    заполнить записями
-    for i in range(1, 11):
-        cursor.execute("INSERT INTO Users(username, email, age, balance) VALUES(?,?,?,1000)",
-                       (f"User{i}", f"example{i}@gmail.com", i * 10))
+def get_all_products():
+    # Выборку всех записей при помощи fetchall(), где возраст не равен 60
+    conn = sqlite3.connect("bot.db")
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM Products ORDER BY id")
+    rows = cursor.fetchall()
+    conn.close()
+    return rows
 
-    #    Обновить баланс у каждой второй записи, начиная с 1-ой (установить на 500)
-    # for i in range(1, 11, 2):
-    #    cursor.execute("UPDATE Users SET balance=? WHERE id=?",(500, i))
-    cursor.execute("UPDATE Users SET balance=? WHERE id % 2 = 1", (500,))
 
-    #    Удалить каждую третью запись, начиная с 1-ой
-    # for i in range(1, 11, 3):
-    #    cursor.execute("DELETE FROM Users WHERE id=?", (i,))
-    cursor.execute("DELETE FROM Users WHERE id % 3 = 1")
-
-    # Удаление пользователя с id=6
-    cursor.execute("DELETE FROM Users WHERE id = ?", (6,))
-
-    # Подсчёт кол-ва всех пользователей
-    cursor.execute("SELECT COUNT(*) FROM Users")
-    total_users = cursor.fetchone()[0]
-
-    # Подсчёт суммы всех балансов
-    cursor.execute("SELECT SUM(balance) FROM Users")
-    all_balances = cursor.fetchone()[0]
-
-    print(all_balances / total_users)
-
+if __name__ == "__main__":
+    # создать таблицу Products
+    initiate_db()
+    # создать 4 записи
+    conn = sqlite3.connect("bot.db")
+    cursor = conn.cursor()
+    # удалить записи, если есть
+    cursor.execute("DELETE FROM Products")
+    # заполнить записями
+    cursor.execute("INSERT INTO Products(title, description, price) VALUES(?,?,?)",
+                   ("Ручка перьевая", "Производитель LAMY", 3000))
+    cursor.execute("INSERT INTO Products(title, description, price) VALUES(?,?,?)",
+                   ("Пинцет", "Made in Pakistan", 500))
+    cursor.execute("INSERT INTO Products(title, description, price) VALUES(?,?,?)",
+                   ("Нож канцелярский", "Силумин, сталь. Сделано в КНР", 1000))
+    cursor.execute("INSERT INTO Products(title, description, price) VALUES(?,?,?)",
+                   ("Ножницы для бумаги", "Производитель Zinger", 2000))
     conn.commit()
     conn.close()
